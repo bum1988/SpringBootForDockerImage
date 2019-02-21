d25e23432dcfc66bf8fa89a6ca06e516783f2aeb
bum1988 <bum1988@naver.com>
2019-02-21 10:39:28 -0800

    Add deploy files
    Add deploy files

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+DOCKER_APP_NAME=app
+
+EXIST_BLUE=$(DOCKER_HOST= docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+
+if [ -z "$EXIST_BLUE" ]; then
+    echo ">>>>>>>>>>>> EXIST BLUE...";
+    DOCKER_HOST= docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml pull
+    DOCKER_HOST= docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d
+
+    sleep 10
+
+    DOCKER_HOST= docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+else
+    echo ">>>>>>>>>>>> EXIST GREEN...";
+    DOCKER_HOST= docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml pull
+    DOCKER_HOST= docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d
+
+    sleep 10
+
+    DOCKER_HOST= docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+fi
