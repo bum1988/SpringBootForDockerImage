c32bece7d1f135cd4e6a3131ad0b10ed34a286ab
bum1988 <bum1988@naver.com>
2019-02-21 19:34:13 +0900

    Add healthCheck method.

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -14,6 +14,11 @@ public class Application {
         return "Hello KaKaoPay Team!!";
     }
 
+    @RequestMapping("/health")
+    public Response healthCheck() {
+        return new Response("success", "It works well!!");
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
